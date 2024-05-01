77fc3593f3cf97faab538db068635566d5bfed08
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-05-01 11:03:19 -0400

    added comments to ProductTag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,39 +1,41 @@
-const { Model, DataTypes } = require('sequelize');
+// Import necessary modules
+const { Model, DataTypes } = require('sequelize'); // Import Model and DataTypes from Sequelize
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection'); // Import sequelize connection
 
-class ProductTag extends Model { }
+class ProductTag extends Model { } // Define ProductTag class which extends Model
 
+// Initialize ProductTag model with its attributes and options
 ProductTag.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    id: { // Define 'id' attribute
+      type: DataTypes.INTEGER, // Data type is INTEGER
+      allowNull: false, // Not null constraint
+      primaryKey: true, // Primary key
+      autoIncrement: true, // Auto-incrementing
     },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
+    product_id: { // Define 'product_id' attribute
+      type: DataTypes.INTEGER, // Data type is INTEGER
+      references: { // Foreign key constraint referencing 'id' column of 'product' table
         model: 'product',
         key: 'id',
       },
     },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
+    tag_id: { // Define 'tag_id' attribute
+      type: DataTypes.INTEGER, // Data type is INTEGER
+      references: { // Foreign key constraint referencing 'id' column of 'tag' table
         model: 'tag',
         key: 'id',
       },
     },
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+    sequelize, // Assign the sequelize connection
+    timestamps: false, // Disable timestamps
+    freezeTableName: true, // Prevents pluralization of table name
+    underscored: true, // Use underscores instead of camelCase for column names
+    modelName: 'product_tag', // Define the model name
   }
 );
 
-module.exports = ProductTag;
+module.exports = ProductTag; // Export the ProductTag model
